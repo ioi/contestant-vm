382049100f2904e950bea55fb2a6072e442d8e2b
Lai Zit Seng <lzs@zitseng.com>
2020-07-17 19:59:47 +0800

    Add suport for a runtime config

--- bin/gnome-autostart.sh
+++ bin/gnome-autostart.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+source /opt/ioi/misc/config
+
 if [ -f /opt/ioi/run/lockscreen ]; then
 	sudo /opt/ioi/bin/lockscreen.sh
 else
-	/opt/ioi/bin/ioisetup
+	if [ "$DOSETUP" = "1" ]; then
+		/opt/ioi/bin/ioisetup
+	fi
 fi
