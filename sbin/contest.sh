0bf1c62e58845c6b3937cef349e64fbd66e50674
Lai Zit Seng <lzs@zitseng.com>
2020-07-29 20:39:34 +0800

    Add monitoring to contest script

--- sbin/contest.sh
+++ sbin/contest.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# This needs to run as root.
+
 contestprep()
 {
 	# Prepare system for contest. This is run BEFORE start of contest.
@@ -28,6 +30,48 @@ contestprep()
 	init 5
 }
 
+schedule()
+{
+	# Remove existing jobs that were created by this script
+	for i in `atq | cut -f1`; do
+		if at -c $i | grep -q '# AUTO-CONTEST-SCHEDULE'; then
+			atrm $i
+		fi
+	done
+
+	while IFS=" " read date time cmd
+	do
+		cat - <<EOM | at $time $date 2> /dev/null
+# AUTO-CONTEST-SCHEDULE
+$cmd
+EOM
+		#echo $date, $time, $cmd
+	done < /opt/ioi/misc/schedule
+}
+
+monitor()
+{
+	DATE=$(date +%Y%m%d%H%M%S)
+	DISPLAY=:0.0 sudo -u ioi xhost +local:root
+	echo "$DATE monitor run" >> /opt/ioi/store/contest.log
+	if [ $(seq 5 | shuf | head -1) -eq 5 ]; then
+		USER=$(cat /opt/ioi/run/userid.txt)
+		DISPLAY=:0.0 xwd -root -silent | convert xwd:- png:- | bzip2 -c - \
+			> /opt/ioi/store/screenshots/$USER-$DATE.png.bz2
+	fi
+	RESOLUTION=$(DISPLAY=:0.0 xdpyinfo | grep dimensions | awk '{print $2}')
+	if [ -f /opt/ioi/run/resolution ]; then
+		if [ "$RESOLUTION" != "$(cat /opt/ioi/run/resolution)" ]; then
+			logger -p local0.alert "Display resolution changed to $RESOLUTION"
+			echo "$RESOLUTION" > /opt/ioi/run/resolution
+		fi
+	else
+		echo "$RESOLUTION" > /opt/ioi/run/resolution
+		logger -p local0.info "Display resolution is $RESOLUTION"
+	fi
+}
+
+
 case "$1" in
 	lock)
 		touch /opt/ioi/run/lockscreen
@@ -39,6 +83,24 @@ case "$1" in
 		;;
 	prep)
 		contestprep
+		;;
+	start)
+		rm /opt/ioi/run/lockscreen
+		systemctl stop i3lock
+		USER=$(/opt/ioi/bin/ioicheckuser)
+		echo "$USER" > /opt/ioi/run/userid.txt
+		echo "$2" >> /opt/ioi/run/contestid.txt
+		echo "* * * * * root /opt/ioi/sbin/contest.sh monitor" > /etc/cron.d/contest
+		;;
+	stop)
+		rm /opt/ioi/run/contestid.txt
+		rm /etc/cron.d/contest
+		;;
+	schedule)
+		schedule
+		;;
+	monitor)
+		monitor
 		;;
 	*)
 		;;
