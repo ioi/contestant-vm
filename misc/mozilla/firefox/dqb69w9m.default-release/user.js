fbce9e01ceb0d347a86656b18e17f0e00aa1f117
Lai Zit Seng <lzs@zitseng.com>
2020-09-04 16:48:51 +0800

    Disable firefox safebrowsing malware/phishing
    This stops firefox delays due to wanting to check downloads

--- misc/mozilla/firefox/dqb69w9m.default-release/user.js
+++ misc/mozilla/firefox/dqb69w9m.default-release/user.js
@@ -1,1 +1,3 @@
 user_pref("browser.startup.homepage", "file:///opt/ioi/html/index.html");
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.safebrowsing.phishing.enabled", false);
