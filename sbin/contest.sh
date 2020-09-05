7fb7fd5727618869f3280cddc381f715e20cae01
Lai Zit Seng <lzs@zitseng.com>
2020-09-05 10:47:23 +0800

    Remove lockscreen in contest done phase

--- sbin/contest.sh
+++ sbin/contest.sh
@@ -120,6 +120,8 @@ case "$1" in
 	done)
 		systemctl stop i3lock
 		/opt/ioi/sbin/firewall.sh stop
+		rm /opt/ioi/run/lockscreen
+		systemctl start i3lock
 		rm /opt/ioi/run/lockdown
 		rm /opt/ioi/run/contestid.txt
 		rm /opt/ioi/run/userid.txt
