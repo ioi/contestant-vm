394a5ee279000c22663d195f8c93ae8059028581
Lai Zit Seng <lzs@zitseng.com>
2021-05-25 11:16:24 +0800

    Reorder where checking of contest schedule is done
    Check time sync first, then check to refresh schedule, then start atd.

--- sbin/startatd.sh
+++ sbin/startatd.sh
@@ -17,5 +17,21 @@ fi
 
 logger -p local0.info "STARTATD: time now is `date`"
 
+# Check for new contest schedule
+SCHEDFILE=$(mktemp)
+wget --timeout=3 --tries=3 -O $SCHEDFILE "${SERVER}/schedule2.txt" > /dev/null 2>&1
+if [ $? -eq 0 -a -f $SCHEDFILE ]; then
+	diff -q /opt/ioi/misc/schedule2.txt $SCHEDFILE > /dev/null
+	if [ $? -ne 0 ]; then
+		logger -p local0.info "STARTATD: Setting up new contest schedule"
+		cp $SCHEDFILE /opt/ioi/misc/schedule2.txt
+		/opt/ioi/sbin/atrun.sh schedule
+	fi
+fi
+rm $SCHEDFILE
+
+logger -p local0.info "STARTATD: Starting atd"
+
 systemctl start atd
 
+# vim: ft=sh ts=4 noet
