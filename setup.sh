bc804731ab4b2a78374d83e7078e413653cb60f6
Lai Zit Seng <lzs@zitseng.com>
2021-06-10 09:44:16 +0800

    Kernel mem param changed to 6144M

--- setup.sh
+++ setup.sh
@@ -149,7 +149,7 @@ chmod 644 /var/lib/AccountsService/users/ansible
 
 # GRUB config: quiet, and password for edit
 
-sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT/ s/"$/ quiet splash maxcpus=2 mem=5G"/' /etc/default/grub
+sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT/ s/"$/ quiet splash maxcpus=2 mem=6144M"/' /etc/default/grub
 GRUB_PASSWD=$(echo -e "$ANSIBLE_PASSWD\n$ANSIBLE_PASSWD" | grub-mkpasswd-pbkdf2 | awk '/hash of / {print $NF}')
 
 sed -i '/\$(echo "\$os" | grub_quote)'\'' \${CLASS}/ s/'\'' \$/'\'' --unrestricted \$/' /etc/grub.d/10_linux
