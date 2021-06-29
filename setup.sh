c6d9fe4d41cfa967fb4fdf45c80185bb8ec22271
Lai Zit Seng <lzs@zitseng.com>
2021-06-30 07:56:19 +0800

    Arg changes to atd startup
    Customise the load and batch arguments.

--- setup.sh
+++ setup.sh
@@ -285,6 +285,26 @@ touch /etc/cloud/cloud-init.disabled
 # Don't stsart atd service 
 systemctl disable atd
 
+# Replace atd.service file
+cat - <<EOM > /lib/systemd/system/atd.service
+[Unit]
+Description=Deferred execution scheduler
+Documentation=man:atd(8)
+After=remote-fs.target nss-user-lookup.target
+
+[Service]
+ExecStartPre=-find /var/spool/cron/atjobs -type f -name "=*" -not -newercc /run/systemd -delete
+ExecStart=/usr/sbin/atd -f -l 5 -b 30
+IgnoreSIGPIPE=false
+KillMode=process
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOM
+
+chmod 644 /lib/systemd/system/atd.service
+
 # Configure systemd for i3lock
 cat - <<EOM > /etc/systemd/system/i3lock.service
 [Unit]
