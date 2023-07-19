655bbbf4a75bd8859c3e90e9ee36f35a544a73de
radl97 <radl97@users.noreply.github.com>
2023-07-19 17:08:39 +0200

    Resolve #23

--- setup.sh
+++ setup.sh
@@ -155,7 +155,7 @@ chmod 644 /var/lib/AccountsService/users/ansible
 
 # GRUB config: quiet, and password for edit
 
-sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT/ s/"$/ quiet splash maxcpus=2 mem=6144M"/' /etc/default/grub
+sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT/ s/"$/ quiet splash"/' /etc/default/grub
 GRUB_PASSWD=$(echo -e "$ANSIBLE_PASSWD\n$ANSIBLE_PASSWD" | grub-mkpasswd-pbkdf2 | awk '/hash of / {print $NF}')
 
 sed -i '/\$(echo "\$os" | grub_quote)'\'' \${CLASS}/ s/'\'' \$/'\'' --unrestricted \$/' /etc/grub.d/10_linux
