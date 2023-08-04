26e8b3571a8a02a6d6221627766a7dfac070a4a4
radl97 <radl97@users.noreply.github.com>
2023-08-04 23:54:58 +0200

    Remove tinc-up: it will be provided by POP server

--- setup.d/21-setup-tinc.sh
+++ setup.d/21-setup-tinc.sh
@@ -9,15 +9,6 @@ set -e
 
 mkdir -p /etc/tinc/vpn
 mkdir -p /etc/tinc/vpn/hosts
-cat - <<'EOM' > /etc/tinc/vpn/tinc-up
-#!/bin/bash
-
-source /opt/ioi/config.sh
-ifconfig $INTERFACE "$(cat /etc/tinc/vpn/ip.conf)" netmask "$(cat /etc/tinc/vpn/mask.conf)"
-route add -net $SUBNET gw "$(cat /etc/tinc/vpn/ip.conf)"
-EOM
-chmod 755 /etc/tinc/vpn/tinc-up
-cp /etc/tinc/vpn/tinc-up /opt/ioi/misc/
 
 cat - <<'EOM' > /etc/tinc/vpn/host-up
 #!/bin/bash
