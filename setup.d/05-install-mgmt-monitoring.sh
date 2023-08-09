6cf83ec5cfa726da0d01358c96c0cff0cbbef782
radl97 <radl97@users.noreply.github.com>
2023-08-09 11:08:43 +0200

    Disable zabbix-agent for now

--- setup.d/05-install-mgmt-monitoring.sh
+++ setup.d/05-install-mgmt-monitoring.sh
@@ -10,3 +10,5 @@ apt -y install net-tools openssh-server ansible xvfb tinc oathtool imagemagick \
 
 # Use a different config for Zabbix
 sed -i '/^Environment=/ s/zabbix_agentd.conf/zabbix_agentd_ioi.conf/' /lib/systemd/system/zabbix-agent.service
+
+systemctl disable zabbix-agent
