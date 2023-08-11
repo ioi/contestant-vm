73c7e46f92478d1e570a0bf5436c1daf4f108403
Baltazár Radics <baltazar.radics@gmail.com>
2023-08-11 20:26:56 +0200

    Update cppreference to 20230810

--- setup.d/16-install-docs.sh
+++ setup.d/16-install-docs.sh
@@ -9,6 +9,6 @@ apt -y install python3-doc
 
 # CPP Reference
 
-$wget https://github.com/PeterFeicht/cppreference-doc/releases/download/v20220730/html-book-20220730.zip
+$wget https://github.com/PeterFeicht/cppreference-doc/releases/download/v20220730/html-book-20230810.zip
 mkdir -p /usr/share/doc/cppreference
-unzip -o $cache/html-book-20220730.zip -d /usr/share/doc/cppreference
+unzip -o $cache/html-book-20230810.zip -d /usr/share/doc/cppreference
